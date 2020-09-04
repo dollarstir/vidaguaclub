deaf9e4e9af4a79c1975748115605ce0718e5584
Dollar Stir <dollarstir@Dollars-MacBook-Pro.local>
2020-09-04 06:36:27 +0000

    first commit

---
+++ core.php
@@ -0,0 +1,268 @@
+<?php
+
+// This website was built by Frederick Ennin(CEO OF Dollarsoft)
+// website: www.dollarstir.com
+// contact +233556676471
+// email:kpin463@gmail.com
+
+function topbar(){
+
+    echo '<div class="header-top-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-12 col-sm-12">
+                <div class="site-info">
+                    <ul>
+                        <li><a href="mailto:mihir_ghosh@live.com"><i class="fa fa-envelope"></i> info@charry.com</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <p class="welcome-message">Welcome to  <b>Vidagua</b> Club</p>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 text-right">
+                <div class="social-icon">
+                    <a href="https://www.facebook.com/search/top?q=vidagua%20foundation"><i class="lab la-facebook-f"></i></a>
+                    <a href="https://www.linkedin.com/public-profile/in/vidagua-foundation-2b17951b3?challengeId=AQE6oXbctYSEEgAAAXQYy8KUuGiocBIjTof1MGUkaKY"><i class="lab la-instagram"></i></a>
+                    <a href="https://mobile.twitter.com/VidaguaUS"><i class="lab la-twitter"></i></a>
+                    <a href="https://www.instagram.com/vidaguafoundation/"><i class="la la-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Header Area -->
+
+<div class="header-area">
+    <div class="sticky-area">
+        <div class="navigation">
+            <div class="row">
+                <div class="col-lg-2">
+                    <div class="logo">
+                        <a class="navbar-brand" href="index.php#"><img src="assets/img/logo.png" alt="" style="width:100px;height:70px;"></a>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="main-menu">
+                        <nav class="navbar navbar-expand-lg">
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                                <span class="navbar-toggler-icon"></span>
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                                <ul class="navbar-nav m-auto">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" href="index.php">Home 
+                                            <span class="sub-nav-toggler"> <i class="fa fa-angle-down"></i>
+                                            </span>
+                                        </a>
+                                        
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="about.php">About Us</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                    <a class="nav-link" href="event.php">Events 
+                                    </a>
+                                   
+                                </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Our Chapters
+                                        </a>
+                                       <!-- <ul class="sub-menu">
+                                            <li><a href="causes.html"> Apply for a Chapter</a></li>
+                                            <li><a href="single-causes.html">Chapters’ Activities</a></li>
+                                        </ul>-->
+                                    </li>
+                                   
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Membership 
+                                        </a>
+                                        <!--<ul class="sub-menu">
+                                            <li><a href="about.html">About us</a></li>
+                                            <li><a href="program.html">Our Programs</a></li>
+                                            <li><a href="team.html">Our Volunteer</a></li>
+                                            <li><a href="portfolio.html">Our Gallery</a></li>
+                                            <li><a href="faq.html">FAQ</a></li>
+                                        </ul>-->
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="index.html#">Project
+                                        Portfolio
+                                        </a>
+                                        <!--<ul class="sub-menu">
+                                            <li><a href="blog.html">Blog</a></li>
+                                            <li><a href="single-blog.html">Blog Details</a></li>
+                                        </ul>-->
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="contact.html">Gallery</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="contact.html"> Our Store</a>
+                                    </li>
+
+                                    
+                                </ul>
+
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-lg-2 text-right">
+                    <div class="header-right-content">
+                        <a href="index.html#" class="main-btn" style="color:white;">Donate Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>';
+
+}
+
+
+function clients(){
+    echo '	<div class="client-area section-padding padding-top-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="logo-carousel owl-carousel">
+                    <div class="single-logo-wrapper">
+                        <div class="logo-inner-item">
+                            <img src="assets/cc/cn.jpg" alt="" style="height:50px;">
+                        </div>
+                    </div>
+                    <div class="single-logo-wrapper">
+                        <div class="logo-inner-item">
+                            <img src="assets/cc/sc.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="single-logo-wrapper">
+                        <div class="logo-inner-item">
+                            <img src="assets/cc/ss.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="single-logo-wrapper">
+                        <div class="logo-inner-item">
+                            <img src="assets/cc/te.jpg" alt="" style="height:60px;">
+                        </div>
+                    </div>
+                    <div class="single-logo-wrapper">
+                        <div class="logo-inner-item">
+                            <img src="assets/img/5.png" alt="">
+                        </div>
+                    </div>
+                    <div class="single-logo-wrapper">
+                        <div class="logo-inner-item">
+                            <img src="assets/cc/ia.jpg" alt="" style="height:60px;">
+                        </div>
+                    </div>
+                    <div class="single-logo-wrapper">
+                        <div class="logo-inner-item">
+                            <img src="assets/cc/am.jpg" alt="" style="height:60px;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+';
+}
+
+function footer(){
+    
+    echo '<footer class="footer-area">
+    <div class="container">
+        <div class="footer-up">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <!-- <div class="logo">
+                        <a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt="" style="height:60px; width:150px;"></a>
+                    </div>
+                    <p>A fresh and modern theme <br>for all charity</p>
+
+                    <div class="contact-info">
+                        <h5>Contact Us</h5>
+                        <p>123 Liverpool Street <br> United Kingdom-1205 </p>
+                    </div> -->
+                    
+
+                    <h5>About Us</h5>
+                    <ul>
+                        <li>
+                            <a href="index.html#">The Water Crisis</a>
+                            <a href="index.html#">Partners</a>
+                            <a href="index.html#">Financials</a>
+                            <a href="index.html#">Contact Us</a>
+                            <a href="index.html#">Meet the Team</a>
+                            <a href="index.html#">Why Water</a>
+
+                            <a href="index.html#">Annual Impact Report</a>
+                            <a href="index.html#">Join Team Water</a>
+                            <a href="index.html#">CSR Engagement</a>
+                            
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6 com-sm-12">
+                    <h5>​Solutions​</h5>
+                    <ul>
+                        <li>
+                            <a href="index.html#">Global Engagement</a>
+                            <a href="index.html#">Monitoring & Evaluation</a>
+                            <a href="index.html#">Water Filtration Systems</a>
+                            
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <h5>​Events, News and Media</h5>
+                    <ul>
+                        <li>
+                            <a href="index.html#">Events & Campaigns</a>
+                            <a href="index.html#">In the News</a>
+                            <a href="index.html#">Media Kit</a>
+                            <!-- <a href="index.html#">Free Eduction</a>
+                            <a href="index.html#">Nutritution Fact</a> -->
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="subscribe-form">
+                        <h5>Subscribe</h5>
+                        <p>Vidagua foundation is a nonproﬁt organization that focuses on delivering clean drinking water to schools, homeless shelters in the United States and in impoverished communities.</p>
+                        <form accept-charset="utf-8" action="https://formspree.io/YOUR_EMAIL_HERE" method="post" id="fs-frm" name="Recieve-Our-Newsletter">
+                           <input type="text" name="name" id="full-name" placeholder="First and Last Name" required="">
+                            <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="">
+                            <button><i class="fa fa-paper-plane"></i></button>
+                        </form>
+                    </div>
+                    
+                    <!-- <table width="135" border="0" cellpadding="2" cellspacing="0" title="Click to Verify - This site chose DigiCert SSL for secure e-commerce and confidential communications."><tr><td width="135" align="center" valign="top"><script type="text/javascript" src="https://seal.websecurity.norton.com/getseal?host_name=www.vidagua.org&amp;size=M&amp;use_flash=NO&amp;use_transparent=No&amp;lang=en"></script><br /><a href="https://www.digicert.com/what-is-ssl-tls-https/" target="_blank"  style="color:#000000; text-decoration:none; font:bold 10px verdana,sans-serif; letter-spacing:.5px;text-align:center; margin:0px; padding:0px;"> How SSL Secures You</a></td></tr></table> -->
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <p class="copyright-line">© 2020 Vidagua. All rights reserved.</p>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <p class="privacy">Privacy Policy | Terms &amp; Conditions</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>';
+}
+
+
+?>
